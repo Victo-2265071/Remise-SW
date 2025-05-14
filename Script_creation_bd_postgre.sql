de8376f44a06973b8f05a798569d55ea8e309690
WilliamChampagne <William_Champagne@hotmail.com>
2025-05-14 13:40:51 -0400

    Nouveau repo

---
+++ Script_creation_bd_postgre.sql
@@ -0,0 +1,35 @@
+-- Supprimer les tables si elles existent
+DROP TABLE IF EXISTS sous_taches;
+DROP TABLE IF EXISTS taches;
+DROP TABLE IF EXISTS utilisateurs;
+ 
+-- Créer la table utilisateurs
+CREATE TABLE utilisateurs (
+	id SERIAL PRIMARY KEY,
+	nom VARCHAR(30),
+	prenom VARCHAR(30),
+	courriel VARCHAR(255),
+	cle_api VARCHAR(30),
+	password VARCHAR(100)
+);
+ 
+-- Créer la table taches
+CREATE TABLE taches (
+	id SERIAL PRIMARY KEY,
+	utilisateur_id INT,
+	titre VARCHAR(100),
+	description VARCHAR(500),
+	date_debut DATE,
+	date_echeance DATE,
+	complete BOOLEAN,
+	FOREIGN KEY (utilisateur_id) REFERENCES utilisateurs(id) ON DELETE CASCADE
+);
+ 
+-- Créer la table sous_taches
+CREATE TABLE sous_taches (
+	id SERIAL PRIMARY KEY,
+	tache_id INT,
+	titre VARCHAR(100),
+	complete BOOLEAN,
+	FOREIGN KEY (tache_id) REFERENCES taches(id) ON DELETE CASCADE
+);
